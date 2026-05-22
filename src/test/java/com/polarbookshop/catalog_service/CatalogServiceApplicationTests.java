74f982b9cdd7c8bd98087f84bd606260d0548c23
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-22 20:24:49 +0500

    chapter 12 integration tests

--- src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/catalog_service/CatalogServiceApplicationTests.java
@@ -1,20 +1,81 @@
 package com.polarbookshop.catalog_service;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.polarbookshop.catalog_service.domain.Book;
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.apache.http.HttpHeaders;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
+@Testcontainers
 class CatalogServiceApplicationTests {
+
+	private static KeycloakToken bjornTokens;
+	private static KeycloakToken isabelleTokens;
 
 	@Autowired
 	private WebTestClient webTestClient;
+
+	@Container
+	private static final KeycloakContainer keycloakContainer =
+			new KeycloakContainer("quay.io/keycloak/keycloak:19.0")
+					.withRealmImportFile("test-realm-config.json");
+
+	@DynamicPropertySource
+	static void dynamicProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.security.oauth2.resourceserver.jwt.issuer-uri",
+				() -> keycloakContainer.getAuthServerUrl() + "realms/PolarBookshop");
+	}
+
+	@BeforeAll
+	static void generateAccessTokens() {
+		WebClient webClient = WebClient.builder()
+				.baseUrl(keycloakContainer.getAuthServerUrl()
+				+ "realms/PolarBookshop/protocol/openid-connect/token")
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.build();
+		isabelleTokens = authenticateWith("isabelle", "password", webClient);
+		bjornTokens = authenticateWith("bjorn", "password", webClient);
+	}
+
+	private static KeycloakToken authenticateWith(String username, String password, WebClient webClient) {
+		return webClient
+				.post()
+				.body(
+						BodyInserters
+								.fromFormData("grant_type", "password")
+								.with("client_id", "polar-test")
+								.with("username", username)
+								.with("password", password)
+				).retrieve()
+				.bodyToMono(KeycloakToken.class)
+				.block();
+	}
+
+	private record KeycloakToken(String accessToken) {
+		@JsonCreator
+		private KeycloakToken(
+				@JsonProperty("access_token") final String accessToken
+		) {
+			this.accessToken = accessToken;
+		}
+	}
 
 	@Test
 	void whenPostRequestThenBookCreated() {
@@ -22,6 +83,7 @@ class CatalogServiceApplicationTests {
 		webTestClient
 				.post()
 				.uri("/books")
+				.headers(headers -> headers.setBearerAuth(isabelleTokens.accessToken()))
 				.bodyValue(expectedBook)
 				.exchange()
 				.expectStatus()
@@ -31,6 +93,26 @@ class CatalogServiceApplicationTests {
 					assertThat(actualBook).isNotNull();
 					assertThat(actualBook.isbn()).isEqualTo(expectedBook.isbn());
 				});
+	}
+
+	@Test
+	void whenPostRequestUnauthorizedThen403() {
+		var expectedBook = Book.of("1231231231", "Title", "Author", 9.90, "Polarsophia");
+
+		webTestClient
+				.post()
+				.uri("/books")
+				.headers(headers -> headers.setBearerAuth(bjornTokens.accessToken()))
+				.bodyValue(expectedBook)
+				.exchange()
+				.expectStatus()
+				.isForbidden();
+	}
+
+	@Test
+	void whenPostRequestUnauthenticatedThen401() {
+		var expectedBook = Book.of("1231231231", "Title", "Author", 9.90, "Polarsophia");
+		webTestClient.post().uri("/books").bodyValue(expectedBook).exchange().expectStatus().isUnauthorized();
 	}
 
 }
