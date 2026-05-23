38821bb012ab45d150e12c20d9025847d39ff598
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-23 20:30:56 +0500

    chapter 13 actuator endpoint all details exposing

--- src/main/java/com/polarbookshop/catalog_service/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/catalog_service/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/", "/books/**")
                         .permitAll()
                         .anyRequest().hasRole("employee")
