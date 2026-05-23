0b7150c25cc5b31247fddfe3ddee88da3efd55d3
Symbat Dulatuly <synbat1999@gmail.com>
2026-05-23 16:55:40 +0500

    chapter 13 logging start

--- src/main/java/com/polarbookshop/catalog_service/web/BookController.java
+++ src/main/java/com/polarbookshop/catalog_service/web/BookController.java
@@ -3,12 +3,16 @@ package com.polarbookshop.catalog_service.web;
 import com.polarbookshop.catalog_service.domain.Book;
 import com.polarbookshop.catalog_service.domain.BookService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private static final Logger log = LoggerFactory.getLogger(BookController.class);
 
     private final BookService bookService;
 
@@ -18,6 +22,7 @@ public class BookController {
 
     @GetMapping
     public Iterable<Book> get() {
+        log.info("Fetching the list of books in the catalog");
         return bookService.viewBookList();
     }
 
